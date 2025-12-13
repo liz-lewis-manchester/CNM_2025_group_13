b91985f0ce6b8987969a7cffa575c1a1d41d1bf4
KatarynaSantana <kataryna.couto@gmail.com>
2025-12-13 19:50:00 +0000

    Update Task_2.py
    Changing variable O to theta as asked in #8 
    Coding for when U is negative as required in #7

--- Task_2.py
+++ Task_2.py
@@ -1,11 +1,15 @@
-Import numpy as np 
+import numpy as np 
 A=np.zeros((N_x-1))
 B=np.zeros((N_x-1))
-O_old=np.zeros((N_x))
-O_new=np.zeros((N_x))
+theta_old=np.zeros((N_x))
+theta_new=np.zeros((N_x))
 
-a= (1/Delta_t) + (U/Delta_x)
-b= -U/Delta_x
+if U > 0:
+  a= (1/Delta_t) + (U/Delta_x)
+  b= -U/Delta_x
+else: 
+  a= (1/Delta_t) - (U/Delta_x)
+  b= -U/Delta_x 
 
 A[:]=a
 B[:]=b
