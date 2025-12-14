64a806c1d72523c63247e0346b8568f06641e5be
KatarynaSantana <kataryna.couto@gmail.com>
2025-12-14 17:53:20 +0000

    Update Task_3.py
    This fixes #9 
    Changes in the forward_substitution was also made, as the set up of the first value for C is from the application of 'apply_inflow_boundary'

--- Task_3.py
+++ Task_3.py
@@ -1,13 +1,14 @@
 # CNM_2025_group_13
 #Task 3
-F=(1/Delta_t)*theta_old[1:] #Setting up right hand side vector F 
+F=(1/Delta_t)*C_current[1:] #Setting up right hand side vector F 
 
-def forward_substitution(A, B, F): #defining a function to solve the linear system 
-  N_x= len(F) +1 #Total grid points, as F is one shorter than theta_old
-  theta_new= np.zeros ((N_x)) #Creating solution vector
-  theta_new[0]=0 #Adding boundary condition at x=0
+def forward_substitution(A, B, F, C_next): #defining a function to solve the linear system 
+  N_x= len(F) +1 #Total grid points, as F is one shorter than C_current
+ 
   
   for i in range (1,N_x): #Creating a loop to solve the linear system 
-    theta_new[i]=(1/(A[i-1]))*(F[i-1]- B[i-1]*theta_new[i-1])
+    C_next[i]=(1/(A[i-1]))*(F[i-1]- B[i-1]*C_next[i-1])
 
-  return theta_new #Setting up the return of the theta_new from this function
+  return C_next #Setting up the return of the C_next from this function
+
+
