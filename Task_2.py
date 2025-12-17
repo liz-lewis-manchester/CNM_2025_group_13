672230e73b887ad35f720db3822c5cb0c0e18ba1
KatarynaSantana <kataryna.couto@gmail.com>
2025-12-17 11:37:36 +0000

    Update Task_2.py
    Adding comments, and deleting placeholder for C_current and C_next, as it was already set up in task 1

--- Task_2.py
+++ Task_2.py
@@ -1,15 +1,17 @@
+#Task 2: storage of matrix 
 import numpy as np 
+#Creating placeholders for the coefficients A and B
 A=np.zeros((N_x-1))
 B=np.zeros((N_x-1))
-C_current=np.zeros((N_x))
-C_next=np.zeros((N_x))
 
+#Setting up the code to work in both positive and negative directiob
 if U > 0:
-  a= (1/Delta_t) + (U/Delta_x)
+  a= (1/Delta_t) + (U/Delta_x) 
   b= -U/Delta_x
 else: 
   a= (1/Delta_t) - (U/Delta_x)
   b= U/Delta_x 
 
+#Setting up the coefficients as constants within the domain as U is constant
 A[:]=a
 B[:]=b
