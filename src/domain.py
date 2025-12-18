7a6b703de26ff54520ae01a7b89c15f498f1eb73
KatarynaSantana <kataryna.couto@gmail.com>
2025-12-18 18:46:38 +0000

    Update domain.py
    Transferring tasks 5 and 6 to this file, so it is more organised

--- src/domain.py
+++ src/domain.py
@@ -1,5 +1,8 @@
 import numpy as np
+import pandas as pd
 
+
+#Task 1:setting up the doamin
 def setup_domain(L, T, Delta_x, Delta_t):
   N_x = int(L / Delta_x) + 1
   N_t = int(T / Delta_t) + 1
@@ -15,13 +18,35 @@ def initial_conditions(N_x, C0, start_index=0):
 
   return C_current, C_next
   
-# This will be used in the testing 
-#L = 20.0
-#T = 5.0 * 60.0
-#Delta_x = 0.2
-#Delta_t = 10.0
-#U = 10.0
-#C0 = 250.0
-#x_array, t_array, N_x, N_t = setup_domain(L, T, Delta_x, Delta_t)
-#C_current, C_next = intial_conditions(N_x, C0, start_index=0)
 
+#Task 5:Reading initial conditions from file 
+def read_initial_conditions(filepath: str) -> pd.DataFrame:
+    """Reads initial pollutant concentrations from a CSV file."""
+    import pandas as pd
+    data = pd.read_csv(filepath)
+    return data
+
+
+#Task 6: Interpolating the data from file into the grid 
+def interpolate_initial_conditions(sensor_data: pd.DataFrame, x_grid: np.ndarray) -> np.ndarray:
+    """
+    Interpolates sensor data onto the model's spatial grid.
+    Args:
+        sensor_data: DataFrame with 'x' (distance) and 'concentration' columns.
+        x_grid: The 1D numpy array representing the model's grid points.
+    Returns:
+        np.ndarray: The pollutant concentration at each point in x_grid.
+    """
+    # Extract sensor data
+    x_sensor = sensor_data['x'].to_numpy() 
+    theta_sensor = sensor_data['concentration'].to_numpy() 
+    sort_indices = np.argsort(x_sensor)
+    x_sensor_sorted = x_sensor[sort_indices]
+    theta_sensor_sorted = theta_sensor[sort_indices]
+    theta_interpolated = np.interp(
+        x_grid, 
+        x_sensor_sorted, 
+        theta_sensor_sorted
+    )
+
+    return theta_interpolated
