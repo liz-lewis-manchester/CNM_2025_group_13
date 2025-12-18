a05fc5e2576dc0180dd63101e082f6f42e6ad8ec
KatarynaSantana <kataryna.couto@gmail.com>
2025-12-18 15:04:08 +0000

    Update domain.py
    While running the test, it was seen a small typo using 'X' instead of 'x'

--- src/domain.py
+++ src/domain.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def setup_domain(L, T, Delta_x, Delta_t):
-  N_x = int(L / Delta_X) + 1
+  N_x = int(L / Delta_x) + 1
   N_t = int(T / Delta_t) + 1
   x_array = np.linspace(0, L, N_x)
   t_array = np.linspace(0, T, N_t)
@@ -14,13 +14,14 @@ def initial_conditions(N_x, C0, start_index=0):
   C_current[start_index]=C0
 
   return C_current, C_next
-
-L = 20.0
-T = 5.0 * 60.0
-Delta_x = 0.2
-Delta_t = 10.0
-U = 10.0
-C0 = 250.0
-x_array, t_array, N_x, N_t = setup_domain(L, T, Delta_x, Delta_t)
-C_current, C_next = intial_conditions(N_x, C0, start_index=0)
+  
+# This will be used in the testing 
+#L = 20.0
+#T = 5.0 * 60.0
+#Delta_x = 0.2
+#Delta_t = 10.0
+#U = 10.0
+#C0 = 250.0
+#x_array, t_array, N_x, N_t = setup_domain(L, T, Delta_x, Delta_t)
+#C_current, C_next = intial_conditions(N_x, C0, start_index=0)
 
