18cf5ff53619044d6ca0da1c2f7ab9dfa8ff6899
KatarynaSantana <kataryna.couto@gmail.com>
2025-12-18 18:47:37 +0000

    Delete Code.py
    This code has now been added into src/ domain.py

--- Code.py
+++
@@ -1,5 +0,0 @@
-def read_initial_conditions(filepath: str) -> pd.DataFrame:
-    """Reads initial pollutant concentrations from a CSV file."""
-    import pandas as pd
-    data = pd.read_csv(filepath)
-    return data
