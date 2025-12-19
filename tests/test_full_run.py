b0cf1faa5b65d21c615aacfe073de7ae9178746f
Alexwong0127 <hon.wong-2@student.manchester.ac.uk>
2025-12-19 10:17:27 +0000

    Delete tests directory

--- tests/test_full_run.py
+++
@@ -1,7 +0,0 @@
-from src.run_simulation import run_simulation
-
-def test_full_run():
-    x, t, C = run_simulation(L=2, dx=1, T=10, dt=5)
-
-    assert C.shape[0] == len(t)
-    assert C.shape[1] == len(x)
